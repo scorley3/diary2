5e677ad130990996e13765699ea219bd7772ad0d
Sam Corley <scorley3@illinois.edu>
2021-10-22 15:25:14 -0500

    added components

--- App.tsx
+++ App.tsx
@@ -11,19 +11,33 @@ export default function App() {
 	</View>
   );
 }
+
+const componentArr = [
+	{ text: "did you brush your teeth today?" , buttonNum : 2, textBox : false},
+	{ text: "did you sleep enough?" , buttonNum : 2, textBox : false},
+	{ text: "what homework do you have?" , buttonNum : 1, textBox : true},
+	{ text: "do you have any tests tomorrow?" , buttonNum : 2, textBox : false},
+	{ text: "how are you feeling today?" , buttonNum : 3, textBox : false},
+	{ text: "what did you eat today?" , buttonNum : 1, textBox : true},
+	{ text: "did you take a shower?" , buttonNum : 2, textBox : false},
+
+
+]
 class MainComponent extends React.Component {
 	render(){
+		var array = componentArr.map(({ text, buttonNum, textBox }) => (
+						<SectionComponent text={text} buttonNum = {buttonNum} textBox = {textBox} style = {styles.section} /> 
+						));
+		
 		return (
 			<View style = {styles.container}>
 				<Button title='new entry' color = '#c71585' margin = {20} borderRadius = {4} />
 				<Button title='past entries' color = '#c71585' />
 				<View style = {styles.frame}>
-					<SectionComponent text = "how are you feeling today?" 
-						buttonNum = {3} textBox = {false} />
-					<SectionComponent text = "what did you eat today?" 
-						buttonNum = {1} textBox = {true} />
-					<SectionComponent text = "did you take a shower?" 
-						buttonNum = {2} textBox = {false} />
+					
+					<View style = {styles.frame}>
+					{array[0]}
+					</View>
 				</View>
 				
 			</View>
@@ -41,6 +55,7 @@ class SectionComponent extends React.Component {
 		};
 	}
 	render() {
+		var buttons = "";
 		if (this.state.buttonNum == 0 & this.state.textBox == false) {
 			return (
 				<View style = {styles.section}>
@@ -55,18 +70,23 @@ class SectionComponent extends React.Component {
 				<Button color = '#c71585' title = 'submit' />
 				</View>
 				);
-		} else if (this.state.textBox == false) {
+		} else if (this.state.textBox == false & this.state.buttonNum == 2) {
+			var arr = ['yes','no'];
+			var buttonArr = [];
+			for (var x = 0; x < arr.length; x++) {
+				buttonArr.push(<Button color = '#c71585' title = {arr[x]} />);
+			}
 			return (
 				<View style = {styles.section}>
 				<Text>{this.state.text}</Text>
-				<Button color = '#c71585' title = 'submit' />
+				{buttonArr}
 				</View>
 			);
 		} else {
 			return (
 				<View style = {styles.section}>
 				<Text>{this.state.text}</Text>
-				<Button color = '#c71585' title = 'submit' />
+				{Array(this.state.buttonNum).fill(<Button color = '#c71585' title = 'maybe' />)}
 				</View>
 			
 			);
@@ -95,7 +115,9 @@ const styles = StyleSheet.create({
 	
   },
   frame: {
-	flexDirection: "row"
+	flexDirection: "row",
+	justifyContent: 'center',
+	alignItems: 'center',
   }
 
 });
